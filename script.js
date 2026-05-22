d7a1e005eb46cf0f2453c78b479a3fbd10451731
rokibalboa9292-gif <rokibalboa9292@gmail.com>
2026-05-23 00:14:28 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1202,7 +1202,7 @@ if(lessonCount <= 2){
 
 const lineHeight = dynamicFontSize + 18;
 
-ctx.font = `${dynamicFontSize}px 'Audiowide', sans-serif`;
+ctx.font = `${dynamicFontSize}px 'Audiowide';
 
 const maxWidth = 940;
 
@@ -1360,7 +1360,7 @@ preparedLessons.forEach((lesson,index)=>{
         lessonFontSize + 18;
 
     ctx.font =
-        `${lessonFontSize}px 'Audiowide', sans-serif`;
+        `${lessonFontSize}px 'Audiowide';
 
     /* KOLOR ZAJĘĆ */
 
